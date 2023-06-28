a2f0b9c965f7a9aeecfcd36d8a915c014020df05
Subhra Dey <sdbappi@Subhras-MBP.attlocal.net>
2023-06-28 13:57:41 -0400

    Using custom library

--- src/main/java/com/ivoyant/learnfactorybean/controller/DeliveryController.java
+++ src/main/java/com/ivoyant/learnfactorybean/controller/DeliveryController.java
@@ -2,9 +2,7 @@ package com.ivoyant.learnfactorybean.controller;
 
 import com.ivoyant.deliverylib.model.DeliveryRequest;
 import com.ivoyant.learnfactorybean.service.DeliveryRegistry;
-import com.ivoyant.sdlibrary.HelloLibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeliveryController {
     @Autowired
     DeliveryRegistry deliveryRegistry;
-    @Autowired
-    HelloLibraryService helloLibraryService;
 
     @PostMapping("/deliver")
     public String deliveryProcess(@RequestBody DeliveryRequest deliveryRequest){
